e0ed3a71b1fe1701659403e5a6cd3a19978b1228
Danny Hill <rdanielhilljr@gmail.com>
2020-02-20 14:05:02 -0800

    adds dailyreportassignment

---
+++ VariablesAndDataTypes/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is a simple program
+
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is " + yourName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //Console.WriteLine("What is your second favorite number?");
+            //string nextNumber = Console.ReadLine();
+            //int secFavNum = Convert.ToInt32(nextNumber);
+            //int numTotal = favNum + secFavNum;
+            //Console.WriteLine("Your two numbers total is :" + numTotal);
+            //Console.ReadLine();
+
+            //double heightInCentimeters = 211.556789797;
+            //float secondsLeft = 2.67f;
+
+            //Console.WriteLine(heightInCentimeters);
+            //Console.WriteLine(secondsLeft);
+            //Console.ReadLine();
+
+            //int myAge = 43;
+            //string myNewAge = Convert.ToString(myAge);
+            //Console.WriteLine("I am " + myNewAge);
+            //Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+       
+
+
+
+
+          
+        }
+    }
+}
---
+++ dailyReportAssignment/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace dailyReportAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy" + Environment.NewLine + "Student Daily Report");
+            Console.WriteLine("What Course are you on?");
+            string myCourse = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            int myPage = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Do you need help with anything? (Please answer true or false)");
+            bool myHelp = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("Was there any positive experiences you would like to share? Please be specific");
+            string myXp = Console.ReadLine();
+            Console.WriteLine("Is there any feedback you would like to provide?");
+            string myFeedBack= Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            int myHours = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("You indicated that you are on the " + myCourse + " course " + " page " + myPage + " and said " + myHelp + " regarding your need for help.");
+            Console.WriteLine("You studied " + myHours + " hours today.");
+            Console.WriteLine("You said your positve experiences were: " + Environment.NewLine + myXp + Environment.NewLine + "Here is the feedback you provided:  " + 
+                Environment.NewLine + myFeedBack);
+
+            Console.WriteLine("Thank you for your answers. An instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+
+
+
+
+            
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
