015ad8976e02bfc43199d15d0988d8ce2c3eb6b2
Danny Hill <rdanielhilljr@gmail.com>
2020-02-23 13:06:42 -0800

    add boolean logic code challenge and practice

---
+++ BooleanCarInsuranceCodeChallenge/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BooleanCarInsuranceCodeChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int myAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? Please answer Yes or No");
+            string myDui = Console.ReadLine();
+            Console.WriteLine("How many speeding tickets do you have?");
+            int mySpeed = Convert.ToInt32(Console.ReadLine());
+
+            bool myApplicant = (myAge > 15 && myDui == "No" && mySpeed < 4);
+            Console.WriteLine("Is this applicant eligible for car insurance? " + myApplicant);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ BooleanLogic/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true && true);
+
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(true || false);
+
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(true == true);
+
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(true != true);
+
+            //Console.WriteLine(true ^ false);
+            //Console.WriteLine(true ^ true);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
