8f3d9f638646efbf9310b5a89d795e576c5b88f7
Danny Hill <rdanielhilljr@gmail.com>
2020-02-27 14:39:01 -0800

    added while loop to coding challenge

--- doWhileLoopProject/Program.cs
+++ doWhileLoopProject/Program.cs
@@ -6,20 +6,31 @@ namespace doWhileLoopProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Guess a number between 1 and 5:");
-            string userNumber = "4";
-            string num;
-            int count = 0;
-            do
-            {
-                num = Console.ReadLine();
-                count++;
-            } while (num != userNumber && count < 2);
+            //    Console.WriteLine("Guess a number between 1 and 5:");
+            //    string userNumber = "4";
+            //    string num;
+            //    int count = 0;
+            //    do
+            //    {
+            //        num = Console.ReadLine();
+            //        count++;
+            //    } while (num != userNumber && count < 2);
 
-            if (num == userNumber)
-                Console.WriteLine("You picked the right number!!");
-            else
-                Console.WriteLine("You didn't pick the right number!");
+            //    if (num == userNumber)
+            //        Console.WriteLine("You picked the right number!!");
+            //    else
+            //        Console.WriteLine("You didn't pick the right number!");
+            //}
+
+            
+            int counter = 0;
+            while (counter < 11)
+            {
+                Console.WriteLine("Tech Academy Rules");
+                counter++;
+            }
+                
+            Console.ReadLine();
         }
     }
 }
