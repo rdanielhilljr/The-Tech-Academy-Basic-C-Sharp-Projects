7293059f20df07f06cc518ef5285a65fcae54c8f
Danny Hill <rdanielhilljr@gmail.com>
2020-02-26 22:58:56 -0800

    doWhileLoop code challenge

---
+++ Loops/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geuss a number?");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            switch (number)
+            {
+                case 62:
+                    Console.WriteLine("You guessed 62. Try again.");
+                    break;
+                case 29:
+                    Console.WriteLine("You guessed 29. Try again.");
+                    break;
+                case 55:
+                    Console.WriteLine("You guessed 55. Try again.");
+                    break;
+                case 12:
+                    Console.WriteLine("You guessed the right number!!");
+                    break;
+                default:
+                    Console.WriteLine("You are wrong.");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ doWhileLoopProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace doWhileLoopProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number between 1 and 5:");
+            string userNumber = "4";
+            string num;
+            int count = 0;
+            do
+            {
+                num = Console.ReadLine();
+                count++;
+            } while (num != userNumber && count < 2);
+
+            if (num == userNumber)
+                Console.WriteLine("You picked the right number!!");
+            else
+                Console.WriteLine("You didn't pick the right number!");
+        }
+    }
+}
