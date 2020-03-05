4b4bc19285ac30c50c243b324e04ee53ae3ce334
Danny Hill <rdanielhilljr@gmail.com>
2020-03-04 17:27:56 -0800

    adds Class Creation and Methods exercise

---
+++ ClassCreationExercise/Computation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassCreationExercise
+{
+    public class Computation
+    {
+        public static int Add(int x, int y)
+        {
+            int Num1 = x;
+            int Num2 = y;
+            int Answer = Num1 + Num2;
+            return Answer;
+        }
+        public static int Subtract(int x, int y)
+        {
+            int Num1 = x;
+            int Num2 = y;
+            int Answer = Num1 - Num2;
+            return Answer;
+        }
+        public static int Multiply(int x, int y)
+        {
+            int Num1 = x;
+            int Num2 = y;
+            int Answer = Num1 * Num2;
+            return Answer;
+        }
+
+    }
+}
---
+++ ClassCreationExercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassCreationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a whole number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a second whole number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int result = Computation.Add(num1, num2);
+            int result2 = Computation.Subtract(num1, num2);
+            int result3 = Computation.Multiply(num1, num2);
+            Console.WriteLine(num1 + " + " + num2 + " = " + result);
+            Console.WriteLine(num1 + " - " + num2 + " = " + result2);
+            Console.WriteLine(num1 + " * " + num2 + " = " + result3);
+            Console.ReadLine();
+
+        }
+        
+    }
+}
