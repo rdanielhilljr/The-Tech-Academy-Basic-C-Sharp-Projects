cf7fd7a5ab6a6339f3041e61219e535dfc2a24f4
Danny Hill <rdanielhilljr@gmail.com>
2020-03-08 16:26:01 -0700

    adds void method assignment

---
+++ VoidMethodAssignment161/Math.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VoidMethodAssignment161
+{
+    class Math
+    {
+        public int num1;
+        public int num2;
+        
+    }
+    
+}
---
+++ VoidMethodAssignment161/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VoidMethodAssignment161
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+
+            math.num2 = 5;
+            math.num1 = 10;
+
+            Console.WriteLine(math.num2 + math.num1);
+            Console.ReadLine();
+        }
+
+    }
+}
