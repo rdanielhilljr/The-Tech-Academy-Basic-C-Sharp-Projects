c302e9e4e50e6badb53bfcbccb2cb415c8c063d0
Danny Hill <rdanielhilljr@gmail.com>
2020-03-08 17:02:51 -0700

    adds corrected assignment Page 160

---
+++ ReduxAssignmentPage160/Computation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReduxAssignmentPage160
+{
+    class Computation
+    {
+        public static int Adds(int x, int y = 5)
+        {
+            int num1 = x;
+            int num2 = y;
+            int result = num1 + num2;
+            return result;
+        }
+    }
+}
---
+++ ReduxAssignmentPage160/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ReduxAssignmentPage160
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter one or two whole numbers (one at a time) to see what your number plus five equals:");
+            int userPick = Convert.ToInt32(Console.ReadLine());
+            int yourNum = Computation.Adds(userPick);
+            Console.WriteLine(userPick + " + 5 = " + yourNum);
+            string userPick2 = Console.ReadLine();
+            if (userPick2 == "")
+            {
+                Console.WriteLine("Thanks, have a great day!");
+            }
+            else
+            {
+                int UserPick3 = Convert.ToInt32(userPick2);
+                int yourNum2 = Computation.Adds(UserPick3);
+                Console.WriteLine(userPick2 + " + 5 = " + yourNum2);
+            }
+            Console.ReadLine();
+        }
+    }
+}
