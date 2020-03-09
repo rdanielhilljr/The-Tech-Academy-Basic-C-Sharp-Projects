7528a3f695d9a9207ffe6dbf0d01593a6155c4d1
Danny Hill <rdanielhilljr@gmail.com>
2020-03-09 16:39:57 -0700

    adds classmethhod164 assignment

---
+++ ClassMethodEx164/Math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodEx164
+{
+    class Math
+    {
+        public void Division(int num1)
+        {
+            int result = num1 / 2;
+            Console.WriteLine("Your number divided by 2 equals: " + result);
+        }
+        
+    }
+}
---
+++ ClassMethodEx164/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ClassMethodEx164
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Math math = new Math();
+            Console.WriteLine("Please enter a whole number:");
+            math.Division(Convert.ToInt32(Console.ReadLine()));
+
+            int a = 25;
+            int b = 50;
+            int sum = 0;
+            int prod = 0;
+            Computation(a, b, out sum, out prod);
+
+            Console.WriteLine("Sum = " + sum);
+            Console.WriteLine("Product = " + prod);
+            Console.ReadLine();
+
+            double c = 25.5;
+            double d = 50.5;
+            double minus = 0;
+            double divis = 0;
+            Computation(c, d, out minus, out divis);
+
+            Console.WriteLine("Minus = " + minus);
+            Console.WriteLine("Division = " + divis);
+            Console.ReadLine();
+
+        }
+
+
+
+        public static void Computation(int x, int y, out int sum, out int prod)
+        {
+                sum = x + y;
+                prod = x * y;
+        }
+        public static void Computation(double x, double y, out double minus, out double divis)
+        {
+            minus = x - y;
+            divis = x / y;
+        }
+
+
+
+
+
+
+
+
+    }
+}
