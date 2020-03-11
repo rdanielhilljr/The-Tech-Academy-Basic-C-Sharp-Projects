e5aca135863b5ec4d6bf24f0dcf36bbfb8b096f9
Danny Hill <rdanielhilljr@gmail.com>
2020-03-11 14:10:02 -0700

    adds overload assignment184

---
+++ OverLoadAssignment184/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OverLoadAssignment184
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+    }
+}
---
+++ OverLoadAssignment184/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OverLoadAssignment184
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            myProgram.DoSomething();
+        }
+
+        public void DoSomething()
+        {
+            Employee emp1 = new Employee();
+            emp1.EmployeeID = 2;
+
+            Employee emp2 = new Employee();
+            emp2.EmployeeID = 1;
+
+            Console.WriteLine(emp1.Equals(emp2));
+            Console.ReadLine();
+        }
+    }
+
+    
+
+        
+    
+}
