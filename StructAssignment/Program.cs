be6a6c6b53ab24ec70c8bf834f151eeb95aeaa7a
Danny Hill <rdanielhilljr@gmail.com>
2020-03-25 13:31:33 -0700

    adds struct assignment

---
+++ StructAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StructAssignment
+{
+    class Program
+    {
+
+    struct Number
+        {
+            public decimal amount { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            
+            Console.WriteLine(number.amount = 3.0M);
+            Console.ReadLine();
+        }
+    }
+}
